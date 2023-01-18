313beb82d42ea45f59dda7cfd2868f90cde5877a
Jens Hilligsøe <jens@hilli.dk>
2023-01-18 11:31:35 +0000

    Add a DB connection check to sample page

--- site/index.php
+++ site/index.php
@@ -16,6 +16,24 @@
   $res = 1 + 1;
   echo "1 + 1 = " . $res;
   ?>
+  <h2>DB check</h2>
+  <?php
+  $servername = "db";
+  $username = "siteuser";
+  $password = "secretpassword";
+  $dbname = "sitedb";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  } else {
+    echo "DB Connections works!";
+  }
+  ?>
+  <h2>php info</h2>
+  <?php phpinfo(); ?>
 </body>
 
 </html>
